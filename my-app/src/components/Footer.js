13bd002f47a787f0a4a255fa6a568028ae36306c
Haley Sulcer <hjsulcer@gmail.com>
2018-11-15 11:12:19 -0500

    added dummy links---and some real ones---to footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -12,24 +12,24 @@ const Footer = () => {
         <div className="left">
             <ul>
                 <span className="footer-title"><li><strong>Company</strong></li></span>
-                <a href="#"><li>About</li></a>
-                <a href="#"><li>Contact</li></a>
-                <a href="#"><li>Press</li></a>
-                <a href="#"><li>Media</li></a>
-                <a href="#"><li>Careers</li></a>
-                <a href="#"><li>Our Team</li></a>
-                <a href="#"><li>Privacy Policy</li></a>
-                <a href="#"><li>Terms of Use</li></a>
+                <a href="https://www.google.com"><li>About</li></a>
+                <a href="https://www.google.com"><li>Contact</li></a>
+                <a href="https://www.google.com"><li>Press</li></a>
+                <a href="https://www.google.com"><li>Media</li></a>
+                <a href="https://www.google.com"><li>Careers</li></a>
+                <a href="https://www.google.com"><li>Our Team</li></a>
+                <a href="https://www.google.com"><li>Privacy Policy</li></a>
+                <a href="https://www.google.com"><li>Terms of Use</li></a>
             </ul>
         </div>
 
         <div className="right">
             <ul>
                 <span className="footer-title"><li><strong>Social</strong></li></span>
-                <a href="#"><li>Twitter</li></a>
-                <a href="#"><li>LinkedIn</li></a>
-                <a href="#"><li>Facebook</li></a>
-                <a href="#"><li>Instagram</li></a>
+                <a href="https://www.google.com"><li>Twitter</li></a>
+                <a href="https://www.linkedin.com/in/haley-sulcer"><li>LinkedIn</li></a>
+                <a href="https://www.google.com"><li>Facebook</li></a>
+                <a href="https://www.instagram.com/hjsulcer"><li>Instagram</li></a>
             </ul>
         </div>
 
